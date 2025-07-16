f29d4ec8b0a57f830da95eb19906f3c52ab26978
seung-ik <69374857+seung-ik@users.noreply.github.com>
2025-07-16 21:18:18 +0900

    fix builderr 3

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 export default nextConfig;
