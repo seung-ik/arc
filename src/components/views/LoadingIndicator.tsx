c93f27530afc504f45353e808df917e6e270fc69
seungik <seungik@seungikui-MacBookPro.local>
2025-08-29 22:29:55 +0900

    fix: loading layout 수정

--- src/components/views/LoadingIndicator.tsx
+++ src/components/views/LoadingIndicator.tsx
@@ -18,7 +18,6 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background-color: rgba(255, 255, 255, 0.9);
   backdrop-filter: blur(5px);
 `;
 
